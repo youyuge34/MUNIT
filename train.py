a4eef5ed1486fa9a4c3d9e69bee5ff8ad179b4ee
yoyo_xps <1197993367@qq.com>
2019-01-06 17:48:32 +0800

    put train into main

--- train.py
+++ train.py
@@ -17,90 +17,94 @@ import sys
 import tensorboardX
 import shutil
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--config', type=str, default='configs/edges2handbags_folder.yaml', help='Path to the config file.')
-parser.add_argument('--output_path', type=str, default='.', help="outputs path")
-parser.add_argument("--resume", action="store_true")
-parser.add_argument('--trainer', type=str, default='MUNIT', help="MUNIT|UNIT")
-opts = parser.parse_args()
+if __name__ == '__main__':
+    main()
 
-cudnn.benchmark = True # trick：加速training
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, default='configs/edges2handbags_folder.yaml', help='Path to the config file.')
+    parser.add_argument('--output_path', type=str, default='.', help="outputs path")
+    parser.add_argument("--resume", action="store_true")
+    parser.add_argument('--trainer', type=str, default='MUNIT', help="MUNIT|UNIT")
+    opts = parser.parse_args()
 
-# Load experiment setting
-config = get_config(opts.config)
-max_iter = config['max_iter']
-display_size = config['display_size']
-config['vgg_model_path'] = opts.output_path
+    cudnn.benchmark = True # trick：加速training
 
-# Setup model and data loader
-if opts.trainer == 'MUNIT':
-    trainer = MUNIT_Trainer(config)
-elif opts.trainer == 'UNIT':
-    trainer = UNIT_Trainer(config)
-elif opts.trainer == 'MOUNT':
-    trainer = MOUNT_Trainer(config)
-else:
-    sys.exit("Only support MUNIT|UNIT|MOUNT")
-trainer.cuda()
-train_loader_a, train_loader_b, test_loader_a, test_loader_b = get_all_data_loaders(config)
-# stack 在 0-dim 产生新的维度
-train_display_images_a = torch.stack([train_loader_a.dataset[i] for i in range(display_size)]).cuda()
-train_display_images_b = torch.stack([train_loader_b.dataset[i] for i in range(display_size)]).cuda()
-test_display_images_a = torch.stack([test_loader_a.dataset[i] for i in range(display_size)]).cuda()
-test_display_images_b = torch.stack([test_loader_b.dataset[i] for i in range(display_size)]).cuda()
+    # Load experiment setting
+    config = get_config(opts.config)
+    max_iter = config['max_iter']
+    display_size = config['display_size']
+    config['vgg_model_path'] = opts.output_path
 
-# Setup logger and output folders
-model_name = os.path.splitext(os.path.basename(opts.config))[0]
-train_writer = tensorboardX.SummaryWriter(os.path.join(opts.output_path + "/logs", model_name))
-output_directory = os.path.join(opts.output_path + "/outputs", model_name)
-checkpoint_directory, image_directory = prepare_sub_folder(output_directory)
-shutil.copy(opts.config, os.path.join(output_directory, 'config.yaml')) # copy config file to output folder
+    # Setup model and data loader
+    if opts.trainer == 'MUNIT':
+        trainer = MUNIT_Trainer(config)
+    elif opts.trainer == 'UNIT':
+        trainer = UNIT_Trainer(config)
+    elif opts.trainer == 'MOUNT':
+        trainer = MOUNT_Trainer(config)
+    else:
+        sys.exit("Only support MUNIT|UNIT|MOUNT")
+    trainer.cuda()
+    train_loader_a, train_loader_b, test_loader_a, test_loader_b = get_all_data_loaders(config)
+    # stack 在 0-dim 产生新的维度
+    train_display_images_a = torch.stack([train_loader_a.dataset[i] for i in range(display_size)]).cuda()
+    train_display_images_b = torch.stack([train_loader_b.dataset[i] for i in range(display_size)]).cuda()
+    test_display_images_a = torch.stack([test_loader_a.dataset[i] for i in range(display_size)]).cuda()
+    test_display_images_b = torch.stack([test_loader_b.dataset[i] for i in range(display_size)]).cuda()
 
-# Start training
-# load模型dict参数（gen,dis,opt*2)，通过iterations重加载schedulers(管理lr)
-iterations = trainer.resume(checkpoint_directory, hyperparameters=config) if opts.resume else 0
-while True:
-    for it, (images_a, images_b) in enumerate(zip(train_loader_a, train_loader_b)):
-        
-        # 使用scheduler逐步降低俩opt中lr的值,仅仅当config：lr_policy ： step 
-        trainer.update_learning_rate()  
+    # Setup logger and output folders
+    model_name = os.path.splitext(os.path.basename(opts.config))[0]
+    train_writer = tensorboardX.SummaryWriter(os.path.join(opts.output_path + "/logs", model_name))
+    output_directory = os.path.join(opts.output_path + "/outputs", model_name)
+    checkpoint_directory, image_directory = prepare_sub_folder(output_directory)
+    shutil.copy(opts.config, os.path.join(output_directory, 'config.yaml')) # copy config file to output folder
 
-        # detach 为了设置照片ab不进行梯度更新 == .require_grad_(False)
-        images_a, images_b = images_a.cuda().detach(), images_b.cuda().detach()
+    # Start training
+    # load模型dict参数（gen,dis,opt*2)，通过iterations重加载schedulers(管理lr)
+    iterations = trainer.resume(checkpoint_directory, hyperparameters=config) if opts.resume else 0
+    while True:
+        for it, (images_a, images_b) in enumerate(zip(train_loader_a, train_loader_b)):
+            
+            # 使用scheduler逐步降低俩opt中lr的值,仅仅当config：lr_policy ： step 
+            trainer.update_learning_rate()  
 
-        # with Timer("Elapsed time in update: %f"):
-            # Main training code，
-            # 正向传播 + 计算各种loss + backward() +opt.step()权重更新
-        trainer.dis_update(images_a, images_b, config)
-        trainer.gen_update(images_a, images_b, config)
-        torch.cuda.synchronize()
+            # detach 为了设置照片ab不进行梯度更新 == .require_grad_(False)
+            images_a, images_b = images_a.cuda().detach(), images_b.cuda().detach()
 
-        # Dump training stats in log file
-        if (iterations + 1) % config['log_iter'] == 0:
-            print("Iteration: %08d/%08d" % (iterations + 1, max_iter))
-            write_loss(iterations, trainer, train_writer)
+            # with Timer("Elapsed time in update: %f"):
+                # Main training code，
+                # 正向传播 + 计算各种loss + backward() +opt.step()权重更新
+            trainer.dis_update(images_a, images_b, config)
+            trainer.gen_update(images_a, images_b, config)
+            torch.cuda.synchronize()
 
-        # Write images
-        if (iterations + 1) % config['image_save_iter'] == 0:
-            with torch.no_grad():
-                # return : x_a, x_a_recon, x_ab1, x_ab2, x_b, x_b_recon, x_ba1, x_ba2
-                test_image_outputs = trainer.sample(test_display_images_a, test_display_images_b)
-                train_image_outputs = trainer.sample(train_display_images_a, train_display_images_b)
-            write_2images(test_image_outputs, display_size, image_directory, 'test_%08d' % (iterations + 1))
-            write_2images(train_image_outputs, display_size, image_directory, 'train_%08d' % (iterations + 1))
-            # HTML
-            # write_html(output_directory + "/index.html", iterations + 1, config['image_save_iter'], 'images')
+            # Dump training stats in log file
+            if (iterations + 1) % config['log_iter'] == 0:
+                print("Iteration: %08d/%08d" % (iterations + 1, max_iter))
+                write_loss(iterations, trainer, train_writer)
 
-        # if (iterations + 1) % config['image_display_iter'] == 0:
-        #     with torch.no_grad():
-        #         image_outputs = trainer.sample(train_display_images_a, train_display_images_b)
-        #     write_2images(image_outputs, display_size, image_directory, 'train_current')
+            # Write images
+            if (iterations + 1) % config['image_save_iter'] == 0:
+                with torch.no_grad():
+                    # return : x_a, x_a_recon, x_ab1, x_ab2, x_b, x_b_recon, x_ba1, x_ba2
+                    test_image_outputs = trainer.sample(test_display_images_a, test_display_images_b)
+                    train_image_outputs = trainer.sample(train_display_images_a, train_display_images_b)
+                write_2images(test_image_outputs, display_size, image_directory, 'test_%08d' % (iterations + 1))
+                write_2images(train_image_outputs, display_size, image_directory, 'train_%08d' % (iterations + 1))
+                # HTML
+                # write_html(output_directory + "/index.html", iterations + 1, config['image_save_iter'], 'images')
 
-        # Save network weights
-        if (iterations + 1) % config['snapshot_save_iter'] == 0:
-            trainer.save(checkpoint_directory, iterations)
+            # if (iterations + 1) % config['image_display_iter'] == 0:
+            #     with torch.no_grad():
+            #         image_outputs = trainer.sample(train_display_images_a, train_display_images_b)
+            #     write_2images(image_outputs, display_size, image_directory, 'train_current')
 
-        iterations += 1
-        if iterations >= max_iter:
-            sys.exit('Finish training')
+            # Save network weights
+            if (iterations + 1) % config['snapshot_save_iter'] == 0:
+                trainer.save(checkpoint_directory, iterations)
+
+            iterations += 1
+            if iterations >= max_iter:
+                sys.exit('Finish training')
 
